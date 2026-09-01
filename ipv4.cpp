cf1f3da3ef18c5a93d39781a8f1eee6f5f458c5f
Chetan Mhavale <cbmhavale21@gmail.com>
2026-08-31 21:39:54 -0700

    Update ipv4.cpp

--- ipv4.cpp
+++ ipv4.cpp
@@ -6,59 +6,48 @@ int main()
 {
     string ip;
     int a, b, c, d;
-
-    cout << "Enter IP address: ";
-    cin >> ip;
-
     char dot;
 
+    cout << "Enter IPv4 address: ";
+    cin >> ip;
+
     stringstream ss(ip);
+    ss >> a >> dot >> b >> dot >> c >> dot >> d;
 
-    if (!(ss >> a >> dot >> b >> dot >> c >> dot >> d) ||
-        dot != '.' ||
-        a < 0 || a > 255 ||
-        b < 0 || b > 255 ||
-        c < 0 || c > 255 ||
-        d < 0 || d > 255)
+    if (a >= 0 && a <= 255 &&
+        b >= 0 && b <= 255 &&
+        c >= 0 && c <= 255 &&
+        d >= 0 && d <= 255)
     {
-        cout << "Invalid IP Address";
-        return 0;
-    }
+        cout << "Valid IP: Yes" << endl;
+        cout << "First Byte: " << a << endl;
 
-    cout << "Valid IP Address" << endl;
-    cout << "First Byte = " << a << endl;
-
-    if (a >= 1 && a <= 126)
-    {
-        cout << "Class = A" << endl;
-        cout << "Network ID = " << a << endl;
-        cout << "Host ID = " << b << "." << c << "." << d << endl;
-    }
-    else if (a >= 128 && a <= 191)
-    {
-        cout << "Class = B" << endl;
-        cout << "Network ID = " << a << "." << b << endl;
-        cout << "Host ID = " << c << "." << d << endl;
-    }
-    else if (a >= 192 && a <= 223)
-    {
-        cout << "Class = C" << endl;
-        cout << "Network ID = " << a << "." << b << "." << c << endl;
-        cout << "Host ID = " << d << endl;
-    }
-    else if (a >= 224 && a <= 239)
-    {
-        cout << "Class = D" << endl;
-        cout << "Network ID and Host ID not applicable" << endl;
-    }
-    else if (a >= 240 && a <= 255)
-    {
-        cout << "Class = E" << endl;
-        cout << "Network ID and Host ID not applicable" << endl;
+        if (a >= 1 && a <= 126)
+        {
+            cout << "Class: A" << endl;
+            cout << "NetID: " << a << ".0.0.0" << endl;
+            cout << "HostID: 0." << b << "." << c << "." << d;
+        }
+        else if (a >= 128 && a <= 191)
+        {
+            cout << "Class: B" << endl;
+            cout << "NetID: " << a << "." << b << ".0.0" << endl;
+            cout << "HostID: 0.0." << c << "." << d;
+        }
+        else if (a >= 192 && a <= 223)
+        {
+            cout << "Class: C" << endl;
+            cout << "NetID: " << a << "." << b << "." << c << ".0" << endl;
+            cout << "HostID: 0.0.0." << d;
+        }
+        else if (a >= 224 && a <= 239)
+            cout << "Class: D";
+        else
+            cout << "Class: E";
     }
     else
     {
-        cout << "Invalid/Reserved IP Address";
+        cout << "Valid IP: No";
     }
 
     return 0;
